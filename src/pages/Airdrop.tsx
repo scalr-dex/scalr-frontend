e178d37694bdd6ed1085224cce03fd94063e924d
T_DaMER <dpancov64@gmail.com>
2024-08-30 20:21:18 +0300

    Finish with tonconnect shit

--- src/pages/Airdrop.tsx
+++ src/pages/Airdrop.tsx
@@ -1,15 +1,44 @@
-import { TonConnectButton, TonConnectUIProvider } from '@tonconnect/ui-react'
+import Logo from 'components/icons/Logo'
+import {
+  BodyText,
+  Header1,
+  Header2,
+  Header3,
+  Header4,
+} from 'components/icons/Text'
 import env from 'helpers/env'
+import {
+  TonConnectButton,
+  TonConnectUIProvider,
+} from 'lib/@tonconnect/ui-react'
 
 export default function () {
   return (
     <TonConnectUIProvider
-      manifestUrl="tonconnect-manifest.json"
+      manifestUrl={`${location.origin}/tonconnect-manifest.json`}
       actionsConfiguration={{
         twaReturnUrl: env.VITE_APP_BASE_LINK,
       }}
+      uiPreferences={{ theme: 'DARK' }}
     >
-      <TonConnectButton />
+      <div className="flex flex-1 flex-col items-center justify-center gap-y-4 text-center px-4">
+        <Header1>Scalr Airdrop</Header1>
+        <Header3>
+          Connect wallet to take part in <span className="underline">$SCR</span>{' '}
+          tokens distribution
+        </Header3>
+        <Logo size={100} withBackground />
+        <TonConnectButton style={{ width: '100%' }} />
+        <span>
+          <Header3>Tokens will be distributed for:</Header3>
+          <Header3 className="font-normal">
+            <ul>
+              <li>- Bet volumes</li>
+              <li>- Invited friends</li>
+            </ul>
+          </Header3>
+        </span>
+      </div>
     </TonConnectUIProvider>
   )
 }
