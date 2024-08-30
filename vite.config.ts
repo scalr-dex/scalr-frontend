e14c70f988d7d3e6c2768b13c6d5a1cba1edac50
T_DaMER <dpancov64@gmail.com>
2024-08-30 15:19:33 +0300

    Add source maps and versioning

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,26 @@ import { defineConfig, Plugin } from 'vite'
 import preact from '@preact/preset-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { visualizer } from 'rollup-plugin-visualizer'
+import { sentryVitePlugin } from '@sentry/vite-plugin'
 
 export default defineConfig({
   plugins: [preact(), tsconfigPaths()],
+  preview: {
+    port: 5173,
+  },
   build: {
+    sourcemap: true, // Source map generation must be turned on
     rollupOptions: {
       plugins: [
         visualizer({
           gzipSize: true,
           brotliSize: true,
-        }) as Plugin,
+        }),
+        sentryVitePlugin({
+          org: 'scalr-15',
+          project: 'scalr-mini-app',
+          authToken: process.env['SENTRY_AUTH_TOKEN'] as string,
+        }),
       ],
     },
     outDir: 'dist',
