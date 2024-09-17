8572fa671309b65683debdaf71392e060656c5b9
T_DaMER <dpancov64@gmail.com>
2024-09-17 16:15:34 +0300

    White eyes and white button

--- src/components/WatchAd.tsx
+++ src/components/WatchAd.tsx
@@ -2,9 +2,10 @@ import { useAdsgram } from 'helpers/hooks/useAdsGram'
 import { toast } from 'react-toastify'
 import env from 'helpers/env'
 import SingleTask from 'components/Tasks/SingleTask'
+import ButtonTypes from 'type/Button'
 
 export default function () {
-  const ad = useAdsgram({
+  const startAd = useAdsgram({
     blockId: env.VITE_ADSGRAM_BLOCK_ID,
     onReward: () => toast.success("You've been rewarded!"),
     onError: () => toast.error('Shoot! An error while watching the ad?!'),
@@ -12,10 +13,11 @@ export default function () {
 
   return (
     <SingleTask
-      icon={<span>AD</span>}
+      icon={<span>👀</span>}
       rewardAmount={750}
       taskText="Watch Ad to get points"
-      onClick={ad}
+      buttonType={ButtonTypes.secondary}
+      onClick={startAd}
     >
       Watch
     </SingleTask>
