65a0b1d7efaaf9c00b60c65cc8dc32e65f8b5693
T_DaMER <dpancov64@gmail.com>
2024-09-17 15:50:41 +0300

    use .env

--- src/components/WatchAd.tsx
+++ src/components/WatchAd.tsx
@@ -1,13 +1,12 @@
 import { useAdsgram } from 'helpers/hooks/useAdsGram'
 import Button from 'components/Button'
 import { toast } from 'react-toastify'
+import env from 'helpers/env'
 
 export default function () {
   const ad = useAdsgram({
-    blockId: '3135',
+    blockId: env.VITE_ADSGRAM_BLOCK_ID,
     onReward: () => toast.success("You've been rewarded!"),
-    onError: () => toast.error('Shooot! An error while watching the ad?!'),
+    onError: () => toast.error('Shoot! An error while watching the ad?!'),
   })
-
-  return <Button onClick={ad}>Watch Ad to get pts</Button>
 }
