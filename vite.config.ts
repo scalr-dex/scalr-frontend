bc5820888e6b03fc4455cb81caf52dbf67ae4aa3
T_DaMER <dpancov64@gmail.com>
2024-09-20 14:40:13 +0300

    Use .env in vite.config.ts to build unminified build, because minified fails on docker

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,34 @@
-import { defineConfig, Plugin } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import preact from '@preact/preset-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { visualizer } from 'rollup-plugin-visualizer'
 import { sentryVitePlugin } from '@sentry/vite-plugin'
 
-export default defineConfig({
-  plugins: [preact(), tsconfigPaths()],
-  preview: {
-    port: 5173,
-  },
-  build: {
-    sourcemap: true,
-    rollupOptions: {
-      plugins: [
-        visualizer({
-          gzipSize: true,
-          brotliSize: true,
-        }),
-        sentryVitePlugin({
-          org: 'scalr-79',
-          project: 'mini-app',
-          authToken: process.env['SENTRY_AUTH_TOKEN'] as string,
-        }),
-      ],
+export default defineConfig(({ mode }) => {
+  const env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''))
+
+  return {
+    plugins: [preact(), tsconfigPaths()],
+    preview: {
+      port: 5173,
     },
-    outDir: 'dist',
-  },
+    build: {
+      sourcemap: true,
+      minify: Boolean(env['MINIFY']),
+      rollupOptions: {
+        plugins: [
+          visualizer({
+            gzipSize: true,
+            brotliSize: true,
+          }),
+          sentryVitePlugin({
+            org: 'scalr-79',
+            project: 'mini-app',
+            authToken: String(env['SENTRY_AUTH_TOKEN']),
+          }),
+        ],
+      },
+      outDir: 'dist',
+    },
+  }
 })
