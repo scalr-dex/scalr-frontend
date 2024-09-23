749e3f412b8d4d6c53652ff518ffe642121c98be
alexkoz <alex.d.kozlov@gmail.com>
2024-09-23 12:28:39 +0300

    added kekokiller logo

--- src/type/UserTask.tsx
+++ src/type/UserTask.tsx
@@ -28,6 +28,7 @@ export const iconNumberToIcon = {
   6: <img src="img/collabs/kolobok.jpg" className="rounded-sm" />,
   7: <img src="img/collabs/makefrens_logo.jpg" className="rounded-sm" />,
   8: <img src="img/collabs/arenagames_logo.png" className="rounded-sm" />,
+  9: <img src="img/collabs/kekokiller_logo.jpg" className="rounded-sm" />,
 } as { [num: number]: JSX.Element }
 
 export const taskStatusToButtonType = {
