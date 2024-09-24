73c53e7df9f70149ede2460a6d8564cb7ae6aabc
T_DaMER <dpancov64@gmail.com>
2024-09-24 20:55:33 +0300

    Reduce font size on big numbers

--- src/components/Main/Points.tsx
+++ src/components/Main/Points.tsx
@@ -1,14 +1,18 @@
 import ScalrCoin from 'components/icons/coins/ScalrCoin'
-import { Header2 } from 'components/Text'
 import CountUp from 'react-countup'
 
 export default function ({ amount = 0 }: { amount?: number | undefined }) {
+  const fontSize = amount >= 10000000 ? 'text-2xl' : 'text-3xl'
+
   return (
     <div className="flex flex-row gap-x-2 items-center">
       <ScalrCoin size={28} />
-      <Header2>
-        <CountUp end={amount} preserveValue duration={1} />
-      </Header2>
+      <CountUp
+        end={amount}
+        preserveValue
+        duration={1}
+        className={`font-bold ${fontSize}`}
+      />
     </div>
   )
 }
