5e3344506751bdeceb65b93252a494e33052626e
T_DaMER <dpancov64@gmail.com>
2024-11-01 19:03:54 +0300

    Change def value

--- src/helpers/atoms/UserStates.ts
+++ src/helpers/atoms/UserStates.ts
@@ -7,9 +7,14 @@ export default atomWithStorage('didOnboard', false, undefined, {
   getOnInit: true,
 })
 
-export const onboardedS2Atom = atomWithStorage('onboardedS2', true, undefined, {
-  getOnInit: true,
-})
+export const onboardedS2Atom = atomWithStorage(
+  'onboardedS2',
+  false,
+  undefined,
+  {
+    getOnInit: true,
+  }
+)
 
 export const showZeroEnergyModal = atom(false)
 export const showDailyStreakModal = atom(false)
