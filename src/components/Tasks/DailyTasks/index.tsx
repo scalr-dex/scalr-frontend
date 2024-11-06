c47747683c1a4309a466dbad0a59c1860bfcfb24
T_DaMER <dpancov64@gmail.com>
2024-11-07 00:17:53 +0300

    [DEPLOY_DEV] import

--- src/components/Tasks/DailyTasks/index.tsx
+++ src/components/Tasks/DailyTasks/index.tsx
@@ -1,11 +1,13 @@
 import AdBlock from 'components/Tasks/DailyTasks/AdBlock'
 import TaskSection from 'components/Tasks/TaskSection'
 import ScalrInName from 'components/Tasks/DailyTasks/ScalrInName'
+import AdBlock2 from 'components/Tasks/DailyTasks/AdBlock2'
 
 export default function () {
   return (
     <TaskSection headerText="Daily tasks">
       <AdBlock />
+      <AdBlock2 />
       <ScalrInName />
     </TaskSection>
   )
